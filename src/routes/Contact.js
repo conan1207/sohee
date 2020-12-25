90f80e1169d58800e6dc87814c61993bdb302e5e
conan1207 <ksohee1207@gmail.com>
2020-12-25 20:53:17 +0900

    text

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,28 +1,25 @@
-import React from "react";
-import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons"
-import { faMobileAlt } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "../css/Contact.scss";
-import face from "../img/about_face_on.png";
+import React from 'react';
+import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../css/Contact.scss';
+import face from '../img/about_face_on.png';
 
 const Contact = () => {
   return (
-    <section className="contact">
-      <img src={face} alt="얼굴" className="face" />
-      <div class="point_txt">
+    <section className='contact'>
+      <img src={face} alt='얼굴' className='face' />
+      <div class='point_txt'>
         <p>달이 떴다고 전화를 주시다니요. &nbsp;</p>
       </div>
       <div>
         <p>
-          <FontAwesomeIcon icon={faMobileAlt} size="2x" color="#24292E" className="mr5"/>
-          010 · 7239 · 3299
+          <FontAwesomeIcon icon={['fab', 'github']} size='2x' color='#24292E' className='mr5' />
+          <a href='https://github.com/conan1207' target='_blank' rel='noopener noreferrer' title='깃허브 새 창 열림'>
+            https://github.com/conan1207
+          </a>
         </p>
         <p>
-          <FontAwesomeIcon icon={["fab", "github"]} size="2x" color="#24292E" className="mr5"/>
-          <a href="https://github.com/conan1207" target="_blank" rel="noopener noreferrer" title="깃허브 새 창 열림">https://github.com/conan1207</a>
-        </p>
-        <p>
-          <FontAwesomeIcon icon={faEnvelopeOpenText} size="2x" color="#24292E" className="mr5"/>
+          <FontAwesomeIcon icon={faEnvelopeOpenText} size='2x' color='#24292E' className='mr5' />
           ksohee1207@gmail.com
         </p>
       </div>
